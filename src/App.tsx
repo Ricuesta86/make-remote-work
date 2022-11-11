684f469c806e4e2946cfbdfd6f52e21803cdb077
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-11 06:38:32 -0500

    Add design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Center, Circle, Stack } from '@chakra-ui/react'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-
-    </div>
+      <Stack direction={{ base: "column", sm: "row" }} spacing="24px">
+        <Center w="40px" h="40px" bg="yellow.200" textAlign={"center"} >
+          1
+        </Center>
+        <Circle size={"40px"} bg="tomato">
+          2
+        </Circle>
+        <Center w="40px" h="40px" bg="pink.100">
+          3
+        </Center>
+      </Stack>
   )
 }
 
